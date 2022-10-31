d5697461bc149b80f6767d0bc71678841bae1a59
MohamedSaadElamir <110488096+MohamedSaadElamir@users.noreply.github.com>
2022-11-01 00:02:28 +0200

    lab2 & validation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +13,8 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('posts', [PostController::class,'index'])->name('posts.index');
-Route::get('posts/create',[PostController::class, 'create'])->name('posts.create');
-Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
-Route::post('posts', [PostController::class, 'store'])->name('posts.store');
+use App\Http\Controllers\ProductController;
+
+Route::resource('product', ProductController::class);
 
 
-// Route::get('posts', [PostController::class, 'index'])->name('posts.index');
