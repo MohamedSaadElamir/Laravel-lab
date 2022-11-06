bd40b312da0e5cff5b63dcfcfcaef6835f5a2813
MohamedSaadElamir <110488096+MohamedSaadElamir@users.noreply.github.com>
2022-11-06 16:36:09 +0200

    last lab

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,27 +1,34 @@
 @extends('layouts.app')
 
-@section('title') create @endsection
+<!-- @section('title') create @endsection -->
 @section('content')
-        <form method="POST" action="{{route('posts.store')}}">
-          @csrf
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">Title</label>
-              <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            </div>
+<form method="POST" enctype="multipart/form-data" action="{{route('posts.store')}}">
+	@csrf
+	@include('layout.error')
+	<div class="mb-3">
+		<label for="exampleInputEmail1" class="form-label">Title</label>
+		<input name="title" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+	</div>
 
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Description</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-              </div>
+	<div class="mb-3">
+		<label for="exampleInputEmail1" class="form-label">Description</label>
+		<input name="description" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+	</div>
 
-              <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Post Creator</label>
-                <select class="form-control">
-                    <option>Mohamed</option>
-                </select>
-              </div>
+	<div class="mb-3">
+		<label for="exampleInputEmail1" class="form-label">Post Creator</label>
+		<select name="post_creator" class="form-control">
+			@foreach ($allUsers as $user)
+			<option value="{{$user->id}}">{{ $user->name }}</option>
+			@endforeach
+		</select>
+	</div>
+	<div class="mb-3">
+		<label for="exampleInputEmail1" class="form-label">image</label>
+		<input type="file" class="form-control" name="image">
+	</div>
 
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </form>
+	<button type="submit" class="btn btn-primary">Submit</button>
+</form>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+use App\Http\Controllers\Auth\SocialAuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,8 +18,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-use App\Http\Controllers\ProductController;
+Route::get('/', function () {
+    return view('welcome');
+});
+/*
+    Important
+*/
+Route::resource('posts', PostController::class)->middleware('auth');
 
-Route::resource('product', ProductController::class);
+Route::post('comments/{postid}', [CommentController::class,'store'])->name('comments.store')->middleware('auth');
+// Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+
+Route::get('/auth/redirect',[SocialAuthController::class,'githubRedirect'])->name('auth.github');
+
+Route::get('/auth/google/redirect',[SocialAuthController::class,'googleRedirect'])->name('auth.google');
+
+Route::get('/auth/callback', [SocialAuthController::class, 'githubCallback']);
+Route::get('/auth/google/callback', [SocialAuthController::class, 'googleCallback']);
 
